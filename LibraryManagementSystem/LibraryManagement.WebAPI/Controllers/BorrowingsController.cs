fe3a455bfa58b49585785b45af1f27b7531130f0
SivalakshmiReddy <sivalakshmireddy07@gmail.com>
2026-04-28 17:33:25 -0400

    Update BorrowingsController.cs

--- LibraryManagementSystem/LibraryManagement.WebAPI/Controllers/BorrowingsController.cs
+++ LibraryManagementSystem/LibraryManagement.WebAPI/Controllers/BorrowingsController.cs
@@ -14,7 +14,7 @@ namespace LibraryManagementSystem.LibraryManagement.WebAPI.Controllers
     {
         private readonly IMediator _mediator;
 
-        public BorrowingsController(IMediator _mediator)
+        public BorrowingsController(IMediator mediator)
         {
             _mediator = mediator;
         }
